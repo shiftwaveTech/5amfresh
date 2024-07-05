5543d6a86a33bdb7b1cd0ecdc736893ac411cb68
shiftwaveTech <hemant@shiftwave.com>
2024-07-05 14:23:25 +0530

    logs

---
+++ application/logs/log-2024-07-05.php
@@ -0,0 +1,54 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_URI::$config is deprecated C:\xampp\htdocs\freshoo\system\core\URI.php 101
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Router::$uri is deprecated C:\xampp\htdocs\freshoo\system\core\Router.php 127
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$benchmark is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$hooks is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$config is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$log is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$utf8 is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$uri is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$exceptions is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$router is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$output is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$security is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$input is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$lang is deprecated C:\xampp\htdocs\freshoo\system\core\Controller.php 82
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$db is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 396
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_DB_mysqli_driver::$failover is deprecated C:\xampp\htdocs\freshoo\system\database\DB_driver.php 371
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$upload is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 1283
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Return type of CI_Session_files_driver::open($save_path, $name) should either be compatible with SessionHandlerInterface::open(string $path, string $name): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice C:\xampp\htdocs\freshoo\system\libraries\Session\drivers\Session_files_driver.php 132
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Return type of CI_Session_files_driver::close() should either be compatible with SessionHandlerInterface::close(): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice C:\xampp\htdocs\freshoo\system\libraries\Session\drivers\Session_files_driver.php 294
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Return type of CI_Session_files_driver::read($session_id) should either be compatible with SessionHandlerInterface::read(string $id): string|false, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice C:\xampp\htdocs\freshoo\system\libraries\Session\drivers\Session_files_driver.php 168
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Return type of CI_Session_files_driver::write($session_id, $session_data) should either be compatible with SessionHandlerInterface::write(string $id, string $data): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice C:\xampp\htdocs\freshoo\system\libraries\Session\drivers\Session_files_driver.php 237
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Return type of CI_Session_files_driver::destroy($session_id) should either be compatible with SessionHandlerInterface::destroy(string $id): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice C:\xampp\htdocs\freshoo\system\libraries\Session\drivers\Session_files_driver.php 317
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Return type of CI_Session_files_driver::gc($maxlifetime) should either be compatible with SessionHandlerInterface::gc(int $max_lifetime): int|false, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice C:\xampp\htdocs\freshoo\system\libraries\Session\drivers\Session_files_driver.php 358
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$session is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 1283
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$parser is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 1283
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$template is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 1283
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$load is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$benchmark is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$hooks is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$config is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$log is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$utf8 is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$uri is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$exceptions is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$router is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$output is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$security is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$input is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$lang is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$db is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$upload is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$session is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$parser is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$template is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$postVals is deprecated C:\xampp\htdocs\freshoo\application\controllers\Welcome.php 28
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$UserModel is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 358
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property Welcome::$data is deprecated C:\xampp\htdocs\freshoo\application\controllers\Welcome.php 34
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$postVals is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$UserModel is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: 8192 --> Creation of dynamic property CI_Loader::$data is deprecated C:\xampp\htdocs\freshoo\system\core\Loader.php 931
+ERROR - 2024-07-05 13:54:34 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory C:\xampp\htdocs\freshoo\application\views\index.php 364
+ERROR - 2024-07-05 13:54:34 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\freshoo\application\views\index.php 364
