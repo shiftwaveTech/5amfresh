6d4990ff6840a72b9167140f29b31707782c1581
sekharbolla07 <sekhar@shiftwave.com>
2024-07-09 14:33:38 +0530

    ci_session

---
+++ application/logs/log-2024-07-09.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-07-09 14:31:17 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:17 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:20 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:20 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:21 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:21 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:21 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:21 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:22 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:22 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:22 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:22 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:23 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:23 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:26 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:26 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:27 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:27 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:27 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:27 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:27 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:27 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:28 --> Severity: Warning --> include(include/footer-main.php): Failed to open stream: No such file or directory G:\wamp\www\5amfresh\application\views\index.php 364
+ERROR - 2024-07-09 14:31:28 --> Severity: Warning --> include(): Failed opening 'include/footer-main.php' for inclusion (include_path='.;C:\php\pear') G:\wamp\www\5amfresh\application\views\index.php 364
